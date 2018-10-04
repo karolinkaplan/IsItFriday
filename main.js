e632af2a60f89700113c404ca3aa21b4314ad8f9
karolin kaplan <karolin.korge@gmail.com>
2018-10-04 18:02:28 +0300

    yo

---
+++ main.js
@@ -0,0 +1,19 @@
+console.log ('YAY')
+
+
+function getDay() {
+    var now = new Date()
+    var day = now.Day();
+
+    if (day === 5) {
+        return 'YES' {
+    else {
+           return 'NO'
+            }
+    }
+
+}
+
+var text = 'Is it Friday'
+
+document.querySelector('h1').innerHTML = text
